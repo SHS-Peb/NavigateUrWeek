65e43f906889596c4c1a5742598ce55a095b5d66
unknown <blackskiesdancer@gmail.com>
2023-07-01 00:25:11 +1000

    Added Map API

--- Project1/Asset/JS/mapBox.js
+++ Project1/Asset/JS/mapBox.js
@@ -1,1 +1,31 @@
 const mapBoxToken = "pk.eyJ1IjoiaG9yYWNlaG91IiwiYSI6ImNsamgwMHVubzBlYnkzZnFnN3U4amZxbmgifQ.mxqdrB6-rH2nfiQ4LA7aug";
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+  })
+  
+  function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+  }
+  
+  function errorLocation() {
+    setupMap([-2.24, 53.48])
+  }
+  
+  function setupMap(center) {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: center,
+      zoom: 15
+    })
+  
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+  
+    var directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+    })
+  
+    map.addControl(directions, "top-left")
+  }
