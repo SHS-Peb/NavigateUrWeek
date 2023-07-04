791fadb65599a4396d97d1a592f7da4e72f59e52
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-07-04 21:21:47 +1000

    cleaned up code in localtionSearch.html & locationSearch.js

--- Project1/Asset/JS/locationSearch.js
+++ Project1/Asset/JS/locationSearch.js
@@ -1,30 +1,11 @@
-// let destinationCardNav = document.getElementById("destinationCardNav");
-
-// destinationCardNav.addEventListener("scroll", (event) => {
-//     console.log("asdf");
-// })
-// $(function () {
-//     $('.toggle-menu').click(function (e) {
-//         e.preventDefault();
-//         $('.sidebar').toggleClass('toggled');
-//     });
-// });
+//---------------- mapbox API -----------------
 
 const mapBoxToken = "pk.eyJ1IjoiaG9yYWNlaG91IiwiYSI6ImNsamgwMHVubzBlYnkzZnFnN3U4amZxbmgifQ.mxqdrB6-rH2nfiQ4LA7aug";
+
 let destinationMap = document.getElementById("destinationMap");
 
-
 mapboxgl.accessToken = mapBoxToken;
-// var map = new mapboxgl.Map({
-//     container: "map",
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     center: [151.2336, -33.8979], // Specify the initial center of the map (e.g., [-122.4194, 37.7749] for San Francisco)
-//     zoom: 10 // Specify the initial zoom level
-// });
 
-// if (!mapboxgl.supported()) {
-//     alert('Your browser does not support Mapbox GL');
-// } else { }
 const map = new mapboxgl.Map({
     container: destinationMap, // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
@@ -48,36 +29,27 @@ const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     types: 'country,region,place,postcode,locality,neighborhood',
     mapboxgl: mapboxgl
-
 });
-
-// geocoder.addTo('#geocoder');
 
 
 // Add geocoder result to container.
-geocoder.on('result', (e) => {
-    // results.innerText = JSON.stringify(e.result, null, 2);
-    // console.log(JSON.stringify(e.result));
-    // console.log(e.result.json());
-
-
-    const data = e.result;
-
+geocoder.on('result', (event) => {
+    const data = event.result;
     // display the image based on the search result
-    getDestinationImage(data.text)
+    getDestinationImage(data.text);
+
+    // getWeatherAPI(data.text);
 
     map.center = data.center;
-    // console.log(e.result);
 });
 
 // Clear results container when search is cleared.
-geocoder.on('clear', () => {
-    // results.innerText = '';
-});
+// geocoder.on('clear', () => {
+//     results.innerText = '';
+// });
 
 // link external geocoder to the map
 document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-
 
 map.addControl(
     new mapboxgl.GeolocateControl({
@@ -99,7 +71,9 @@ map.addControl(
 );
 
 
-// UI
+
+//---------------- UI -----------------
+
 var button = document.querySelector('.desti-route-button');
 
 button.addEventListener('click', () => {
@@ -109,10 +83,38 @@ button.addEventListener('click', () => {
     }, 200);
 });
 
-let destinationImage = document.getElementById("destinationImage");
 
+let options = {
+    accessibility: true,
+    prevNextButtons: true,
+    pageDots: true,
+    setGallerySize: false,
+    arrowShape: {
+        x0: 10,
+        x1: 60,
+        y1: 50,
+        x2: 60,
+        y2: 45,
+        x3: 15
+    }
+};
+
+let carousel = document.querySelector('[data-carousel]');
+let slides = document.getElementsByClassName('carousel-cell');
+let flkty = new Flickity(carousel, options);
+
+flkty.on('scroll', () => {
+    // console.log(slides.length);
+    flkty.slides.forEach((slide, i) => {
+        let image = slides[i];
+        let x = (slide.target + flkty.x) * -1 / 3;
+        image.style.backgroundPosition = x + 'px';
+    });
+});
+
+
+//---------------- Unsplash API-----------------
 const client_id = "g9ZK5ag6po5d9rJet7HIMBi2dJdI4GDcL2KoZyZDOyg";
-// let destination = "Sydney";
 let page = "1";
 let per_page = "7";
 
@@ -124,20 +126,19 @@ let getDestinationImage = (destination) => {
     fetch(apiUrl)
         .then((response) => {
             if (response.ok) {
-                // console.log(response.j);
+
                 response.json().then((data) => {
 
-
+                    console.log(data);
                     for (let i = 0; i < slides.length; i++) {
-                        // const element = array[index];
                         let url = data.results[i].urls.regular;
                         let alt_description = data.results[i].alt_description;
 
                         slides[i].style.backgroundImage = `url(${url})`;
                         titles[i].textContent = alt_description;
+                        // titles[i].textContent = "";
                         console.log(url);
                     }
-
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -147,3 +148,4 @@ let getDestinationImage = (destination) => {
             alert('Unable to connect');
         });
 };
+
