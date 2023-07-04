4a0687c668d258fb6f8afa70bfccc9669a7424f9
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-07-04 19:56:01 +1000

    updated functions

--- Project1/Asset/JS/locationSearch.js
+++ Project1/Asset/JS/locationSearch.js
@@ -114,20 +114,30 @@ let destinationImage = document.getElementById("destinationImage");
 const client_id = "g9ZK5ag6po5d9rJet7HIMBi2dJdI4GDcL2KoZyZDOyg";
 // let destination = "Sydney";
 let page = "1";
-let per_page = "5";
+let per_page = "7";
 
 let getDestinationImage = (destination) => {
-    var apiUrl = `https://api.unsplash.com/search/photos/?page=${page}&per_page=${per_page}&query=${destination}&client_id=${client_id}`;
+    let apiUrl = `https://api.unsplash.com/search/photos/?page=${page}&per_page=${per_page}&query=${destination}&client_id=${client_id}`;
+    let slides = document.getElementsByClassName('carousel-cell');
+    let titles = document.getElementsByClassName('title');
 
     fetch(apiUrl)
         .then((response) => {
             if (response.ok) {
                 // console.log(response.j);
                 response.json().then((data) => {
-                    imageUrl = data.results[0].urls.regular;
-                    console.log(imageUrl);
-                    // displayRepos(data, user);
-                    destinationImage.src = imageUrl;
+
+
+                    for (let i = 0; i < slides.length; i++) {
+                        // const element = array[index];
+                        let url = data.results[i].urls.regular;
+                        let alt_description = data.results[i].alt_description;
+
+                        slides[i].style.backgroundImage = `url(${url})`;
+                        titles[i].textContent = alt_description;
+                        console.log(url);
+                    }
+
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -137,32 +147,3 @@ let getDestinationImage = (destination) => {
             alert('Unable to connect');
         });
 };
-
-// getDestinationImage();
-
-// var options = {
-//     accessibility: true,
-//     prevNextButtons: true,
-//     pageDots: true,
-//     setGallerySize: false,
-//     arrowShape: {
-//         x0: 10,
-//         x1: 60,
-//         y1: 50,
-//         x2: 60,
-//         y2: 45,
-//         x3: 15
-//     }
-// };
-
-// var carousel = document.querySelector('[data-carousel]');
-// var slides = document.getElementsByClassName('carousel-cell');
-// var flkty = new Flickity(carousel, options);
-
-// flkty.on('scroll', function () {
-//     flkty.slides.forEach(function (slide, i) {
-//         var image = slides[i];
-//         var x = (slide.target + flkty.x) * -1 / 3;
-//         image.style.backgroundPosition = x + 'px';
-//     });
-// });
